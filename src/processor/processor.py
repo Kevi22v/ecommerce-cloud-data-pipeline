0a047b41aa885547d1179aeed0d18d113926f82b
Venkata Naga Sai Vivek Kalipatnapu <vivekalipatnam@gmail.com>
2026-04-08 20:00:59 +0530

    fix: processor for dashboard

--- src/processor/processor.py
+++ src/processor/processor.py
@@ -6,9 +6,9 @@ from pyspark.sql.types import StructType, StructField, StringType, DoubleType, T
 # 1. Initialize the Spark Session
 spark = SparkSession.builder \
     .appName("EcommerceRealTimeProcessor") \
-    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262") \
+    .config("spark.jars.packages", "org.apache.spark:spark-sql-kafka-0-10_2.12:3.4.1,org.apache.hadoop:hadoop-aws:3.3.4,com.amazonaws:aws-java-sdk-bundle:1.12.262,org.postgresql:postgresql:42.6.0") \
     .getOrCreate()
-    
+
 spark.sparkContext.setLogLevel("WARN")
 
 # Fetch database credentials securely injected by Kubernetes ConfigMap and Secret
